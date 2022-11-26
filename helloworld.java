8f0aedafb896e0e6887c785d0e00e2613f229721
Chandrakanth356 <118492266+Chandrakanth356@users.noreply.github.com>
2022-11-26 20:22:32 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Made some changes!"); 
+        System.out.println("Hello, edited using webhook!"); 
     }
 }
